8804f41c3419cecff4b7362ed0a1d48b1c7e6e7f
ViktorRigs <116713542+ViktorRigs@users.noreply.github.com>
2024-01-16 23:12:22 +0300

    Try 'Ping' Commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,23 @@ import { Client } from 'discord.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const client = new Client({ intents: [] });
+const client = new Client({
+    intents: [
+        "Guilds",
+        "GuildMessages"
+    ]
+});
 
 client.once('ready', () => {
     console.log('Bot is ready!');
 });
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN);
