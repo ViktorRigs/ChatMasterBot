17b185ca2fcb85c361ddb650c8c54e8f966cff15
ViktorRigs <116713542+ViktorRigs@users.noreply.github.com>
2024-01-17 21:34:49 +0300

    Initial project setup: Added Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: "standard-with-typescript",
+    ignorePatterns: [".eslintrc.js", "dist/*"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        '@typescript-eslint/semi': ['error', 'always'], // or 'never' if you prefer no semicolons
+    },
+};
