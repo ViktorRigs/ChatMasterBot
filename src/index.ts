564d5839eb6d4caf0416383a371c77c61f0e4396
ViktorRigs <116713542+ViktorRigs@users.noreply.github.com>
2024-01-17 21:51:38 +0300

    Fixed intents.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,37 @@
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits, Events } from 'discord.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 const client = new Client({
-    intents: [
-        "Guilds",
-        "GuildMessages"
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once('ready', () => {
-    console.log('Bot is ready!');
+  console.log('Bot is ready!');
+});
+
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
+  if (message.author.bot) return;
 
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
-})
+  if (message.content === 'ping') {
+    message.reply('pong').catch(console.error);
+  }
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN).catch(console.error);
